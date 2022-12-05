02d068fec3da10c060beeaf4f4f4833350c101f7
esaltzm <elisaltzman@gmail.com>
2022-12-05 16:36:40 -0500

    added CORS

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express')
-
+const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const app = express()
 
-
 const restaurantController = require('./controllers/Restaurant')
 
-
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/api/restaurants/', restaurantController)
@@ -15,4 +14,5 @@ app.use('/api/restaurants/', restaurantController)
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
+
 app.listen(4000, () => console.log('Server running on port 4000!'))
